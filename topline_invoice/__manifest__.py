54355d03c7ecc59c206e03faedac4cf65b9daa62
Olalekan Babawale <padinality@yahoo.com>
2024-02-20 15:17:29 +0100

    Added invoice template modification module

---
+++ topline_invoice/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Invoice Modification",
+
+    'summary': """
+        remove background color from total line on invoice""",
+
+    'description': """
+        remove background color from total line on invoice
+    """,
+
+    'author': "HyperIT Consultants",
+    'website': "http://www.mceesolutions.com",
+
+    'category': 'Accounting/Accounting',
+    'version': '0.1',
+
+    'depends': [
+        'account',
+    ],
+
+    'license': 'LGPL-3',
+
+    'data': [
+        'views/invoice_template.xml'
+    ]
+}
