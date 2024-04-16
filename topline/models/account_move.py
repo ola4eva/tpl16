11bb102fafae9cec0e685aa957baa1e10a953f65
Olalekan Babawale <padinality@yahoo.com>
2024-04-16 20:23:10 +0100

    Fixed payment bug

--- topline/models/account_move.py
+++ topline/models/account_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 from odoo.tools import float_compare
 
 
@@ -10,11 +10,9 @@ class AccountMoveLine(models.Model):
     payment_requisition_id = fields.Many2one(
         'payment.requisition.form', string='Payment Requisition', copy=False, help="Payment Requisition where the move line come from")
 
-    def reconcile(self, writeoff_acc_id=False, writeoff_journal_id=False):
-        res = super(AccountMoveLine, self).reconcile(
-            writeoff_acc_id=writeoff_acc_id, writeoff_journal_id=writeoff_journal_id)
-        account_move_ids = [l.move_id.id for l in self if float_compare(
-            l.move_id.matched_percentage, 1, precision_digits=5) == 0]
+    def reconcile(self):
+        res = super(AccountMoveLine, self).reconcile()
+        account_move_ids = [l.move_id.id for l in self]
         if account_move_ids:
             expense_sheets = self.env['hr.expense.sheet'].search([
                 ('account_move_id', 'in', account_move_ids), ('state', '!=', 'done')
