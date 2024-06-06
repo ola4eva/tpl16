4805e952ab8700a606ae728ed8da6b04b8f12297
Olalekan Babawale <padinality@yahoo.com>
2024-06-06 13:39:33 +0100

    [FIX] topline_store_request: Fixed migration issues with Store request

--- topline_store_request/models/stock_picking.py
+++ topline_store_request/models/stock_picking.py
@@ -3,156 +3,144 @@
 from datetime import date
 from odoo.tools import float_is_zero
 from odoo.exceptions import UserError
-from odoo import api, fields, models, _
+from odoo import fields, models, api, _
 
-# class Picking(models.Model):
-#     _name = "stock.picking"
-#     _inherit = 'stock.picking'
-#     _order = 'create_date DESC'
 
-#     state = fields.Selection([
-#         ('draft', 'Draft'),
-#         ('submit', 'Submitted'),
-#         ('approve', 'Line Manager Approved'),
-#         ('qa_qc_approve', 'QA/QC Approved'),
-#         ('waiting', 'Waiting Another Operation'),
-#         ('confirmed', 'Waiting'),
-#         ('assigned', 'Ready'),
-#         ('done', 'Done'),
-#         ('reject', 'Rejected'),
-#         ('cancel', 'Cancelled'),
-#     ], string='Status',
-#         # compute='_compute_state',
-#         copy=False, index=True, readonly=True, store=True, tracking=True,
-#         help=" * Draft: not confirmed yet and will not be scheduled until confirmed.\n"
-#              " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows).\n"
-#              " * Waiting: if it is not ready to be sent because the required products could not be reserved.\n"
-#              " * Ready: products are reserved and ready to be sent. If the shipping policy is 'As soon as possible' this happens as soon as anything is reserved.\n"
-#              " * Done: has been processed, can't be modified or cancelled anymore.\n"
-#              " * Cancelled: has been cancelled, can't be confirmed anymore.")
-#     picking_type_name = fields.Char(
-#         string='Picking Type Name', related='picking_type_id.name')
-#     active = fields.Boolean('Active', default=True)
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+    _order = 'create_date DESC'
 
-    
-#     def unlink(self):
-#         for picking in self:
-#             store_request_picking_type = self.env.ref(
-#                 "topline.stock_picking_type_emp")
-#             if picking.picking_type_id == store_request_picking_type:
-#                 raise UserError(
-#                     "You are not allowed to delete store requests, you may consider archiving instead!!!")
-#         return super(Picking, self).unlink()
+    def _default_owner(self):
+        return self.env.context.get('default_employee_id') or self.env['res.users'].browse(self.env.uid).partner_id
 
-#     def button_submit(self):
-#         self.write({'state': 'submit'})
-#         for move in self.move_ids_without_package:
-#             move.state = 'submit'
-#         partner_ids = []
-#         if self.employee_id.parent_id.user_id:
-#             partner_ids.append(
-#                 self.employee_id.parent_id.user_id.partner_id.id)
-#         self.message_subscribe(partner_ids=partner_ids)
-#         subject = "Store request {} for {} needs approval".format(
-#             self.name, self.employee_id.name)
-#         self.message_post(subject=subject, body=subject,
-#                           partner_ids=partner_ids)
-#         return False
+    def _default_employee(self):
+        self.env['hr.employee'].search([('user_id', '=', self.env.uid)])
+        return self.env['hr.employee'].search([('user_id', '=', self.env.uid)])
 
-#     def action_confirm(self):
-#         self.write({'is_locked': True})
-#         for move in self.move_ids_without_package:
-#             move.state = 'confirmed'
-#         res = super(Picking, self).action_confirm()
-#         if self.picking_type_id.name == 'Staff Store Requests':
-#             self.button_approve_srt()
-#             group_id = self.env.ref(
-#                 'stock.group_stock_manager')
-#             user_ids = []
-#             partner_ids = []
-#             for user in group_id.users:
-#                 user_ids.append(user.id)
-#                 partner_ids.append(user.partner_id.id)
-#             self.message_subscribe(partner_ids=partner_ids)
-#             subject = "Store request {} has been authorized".format(self.name)
-#             self.message_post(subject=subject, body=subject,
-#                               partner_ids=partner_ids)
-#             return False
-#         return res
+    owner_id = fields.Many2one('res.partner', 'Owner',
+                               states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_owner,
+                               help="Default Owner")
+    employee_id = fields.Many2one('hr.employee', 'Requesting Employee',
+                                  states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_employee,
+                                  help="Default Owner")
+    request_date = fields.Date(string='Date', default=date.today())
+    department_id = fields.Many2one(
+        comodel_name='hr.department', string='Department', related='employee_id.department_id')
 
-#     def action_line_manager_approval(self):
-#         self.write({'state': 'approve'})
-#         group_id = self.env.ref(
-#             'topline.group_qa')
-#         for move in self.move_ids_without_package:
-#             move.state = 'approve'
-#         self.manager_confirm()
-#         subject = "Store request {} for {} has been approved by line manager".format(
-#             self.name, self.employee_id.name)
-#         partner_ids = []
-#         user_ids = []
-#         for user in group_id.users:
-#             user_ids.append(user.id)
-#             partner_ids.append(user.partner_id.id)
-#         self.message_post(subject=subject, body=subject,
-#                           partner_ids=partner_ids)
+    state = fields.Selection(selection_add=[
+        ('submit', 'Submitted'),
+        ('approve', 'Line Manager Approved'),
+        ('qa_qc_approve', 'QA/QC Approved'),
+        ('waiting',),
+        ('reject', 'Rejected'),
+        ('cancel',)
+    ])
+    picking_type_name = fields.Char(
+        string='Picking Type Name', related='picking_type_id.name')
+    active = fields.Boolean('Active', default=True)
 
-#     def action_qa_qc_approval(self):
-#         self.write({'state': 'qa_qc_approve'})
-#         for move in self.move_ids_without_package:
-#             move.state = 'qa_qc_approve'
-#         self.manager_confirm()
-#         subject = "Store request {} for {} has been approved by QA/QC".format(
-#             self.name, self.employee_id.name)
-#         partner_ids = []
-#         for partner in self.message_partner_ids:
-#             partner_ids.append(partner.id)
-#         self.message_post(subject=subject, body=subject,
-#                           partner_ids=partner_ids)
-#         self.send_store_request_mail()
+    total_price = fields.Float(
+        string='Total',
+        compute='_total_price',
+        readonly=True, store=True)
+    man_confirm = fields.Boolean(
+        'Manager Confirmation', tracking=True)
+    client_id = fields.Many2one(
+        'res.partner', string='Client', index=True, ondelete='cascade', required=False)
+    need_approval = fields.Boolean(
+        'Need Approval', tracking=True, copy=False)
+    total_cost = fields.Float(
+        string='Total Cost', compute='_total_cost', tracking=True, readonly=True)
+    project_id = fields.Many2one(
+        'project.project', string='Project', index=True, ondelete='cascade', required=False)
+    project_description = fields.Char('Project Description', copy=False)
 
-#     def manager_confirm(self):
-#         for order in self:
-#             order.write({'man_confirm': True})
-#         return True
+    rejection_reason = fields.Many2one(
+        'stock.rejection.reason', string='Rejection Reason', index=True, tracking=True)
 
-#     def _default_owner(self):
-#         return self.env.context.get('default_employee_id') or self.env['res.users'].browse(self.env.uid).partner_id
+    def do_nothing(self):
+        pass
 
-#     def _default_employee(self):
-#         self.env['hr.employee'].search([('user_id', '=', self.env.uid)])
-#         return self.env['hr.employee'].search([('user_id', '=', self.env.uid)])
+    def unlink(self):
+        for picking in self:
+            store_request_picking_type = self.env.ref(
+                "topline.stock_picking_type_emp")
+            if picking.picking_type_id == store_request_picking_type:
+                raise UserError(
+                    "You are not allowed to delete store requests, you may consider archiving instead!!!")
+        return super(Picking, self).unlink()
 
-#     owner_id = fields.Many2one('res.partner', 'Owner',
-#                                states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_owner,
-#                                help="Default Owner")
+    def button_submit(self):
+        self.write({'state': 'submit'})
+        for move in self.move_ids_without_package:
+            move.state = 'submit'
+        partner_ids = []
+        if self.employee_id.parent_id.user_id:
+            partner_ids.append(
+                self.employee_id.parent_id.user_id.partner_id.id)
+        self.message_subscribe(partner_ids=partner_ids)
+        subject = "Store request {} for {} needs approval".format(
+            self.name, self.employee_id.name)
+        self.message_post(subject=subject, body=subject,
+                          partner_ids=partner_ids)
+        return False
 
-#     employee_id = fields.Many2one('hr.employee', 'Requesting Employee',
-#                                   states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_employee,
-#                                   help="Default Owner")
+    def action_confirm(self):
+        self.write({'is_locked': True})
+        for move in self.move_ids_without_package:
+            move.state = 'confirmed'
+        res = super(Picking, self).action_confirm()
+        if self.picking_type_id.name == 'Staff Store Requests':
+            self.button_approve_srt()
+            group_id = self.env.ref(
+                'stock.group_stock_manager')
+            user_ids = []
+            partner_ids = []
+            for user in group_id.users:
+                user_ids.append(user.id)
+                partner_ids.append(user.partner_id.id)
+            self.message_subscribe(partner_ids=partner_ids)
+            subject = "Store request {} has been authorized".format(self.name)
+            self.message_post(subject=subject, body=subject,
+                              partner_ids=partner_ids)
+            return False
+        return res
 
-#     request_date = fields.Date(string='Date', default=date.today())
-#     department_id = fields.Many2one(
-#         comodel_name='hr.department', string='Department', related='employee_id.department_id')
+    def action_line_manager_approval(self):
+        self.write({'state': 'approve'})
+        group_id = self.env.ref(
+            'topline.group_qa')
+        for move in self.move_ids_without_package:
+            move.state = 'approve'
+        self.manager_confirm()
+        subject = "Store request {} for {} has been approved by line manager".format(
+            self.name, self.employee_id.name)
+        partner_ids = []
+        user_ids = []
+        for user in group_id.users:
+            user_ids.append(user.id)
+            partner_ids.append(user.partner_id.id)
+        self.message_post(subject=subject, body=subject,
+                          partner_ids=partner_ids)
 
-#     total_price = fields.Float(
-#         string='Total',
-#         compute='_total_price',
-#         readonly=True, store=True)
-#     man_confirm = fields.Boolean(
-#         'Manager Confirmation', tracking=True)
-#     client_id = fields.Many2one(
-#         'res.partner', string='Client', index=True, ondelete='cascade', required=False)
-#     need_approval = fields.Boolean(
-#         'Need Approval', tracking=True, copy=False)
-#     total_cost = fields.Float(
-#         string='Total Cost', compute='_total_cost', tracking=True, readonly=True)
-#     project_id = fields.Many2one(
-#         'project.project', string='Project', index=True, ondelete='cascade', required=False)
-#     project_description = fields.Char('Project Description', copy=False)
+    def action_qa_qc_approval(self):
+        self.write({'state': 'qa_qc_approve'})
+        for move in self.move_ids_without_package:
+            move.state = 'qa_qc_approve'
+        self.manager_confirm()
+        subject = "Store request {} for {} has been approved by QA/QC".format(
+            self.name, self.employee_id.name)
+        partner_ids = []
+        for partner in self.message_partner_ids:
+            partner_ids.append(partner.id)
+        self.message_post(subject=subject, body=subject,
+                          partner_ids=partner_ids)
+        self.send_store_request_mail()
 
-#     rejection_reason = fields.Many2one(
-#         'stock.rejection.reason', string='Rejection Reason', index=True, tracking=True)
+    def manager_confirm(self):
+        for order in self:
+            order.write({'man_confirm': True})
+        return True
 
 #     @api.depends('move_ids_without_package.product_uom_qty')
 #     def _total_cost(self):
@@ -209,47 +197,52 @@ from odoo import api, fields, models, _
 #         self.need_approval = False
 #         return {}
 
-#     @api.depends('move_ids_without_package.price_unit')
-#     def _total_price(self):
-#         total_price = 0.0
-#         for rec in self:
-#             for line in rec.move_ids_without_package:
-#                 total_price += line.price_subtotal
-#             rec.total_price = total_price
 
-#     def create_atp_order(self):
-#         """
-#         Method to open create atp form
-#         """
-#         view_ref = self.env['ir.model.data'].get_object_reference(
-#             'topline', 'topline_atp_form_view')
-#         view_id = view_ref[1] if view_ref else False
+    @api.depends('move_ids_without_package.price_unit')
+    def _total_price(self):
+        total_price = 0.0
+        for rec in self:
+            for line in rec.move_ids_without_package:
+                total_price += line.price_subtotal
+            rec.total_price = total_price
 
-#         for subscription in self:
-#             order_lines = []
-#             for line in subscription.move_ids_without_package:
-#                 order_lines.append((0, 0, {
-#                     'name': line.product_id.name,
-#                     'product_id': line.product_id.id,
-#                     'qty': line.product_uom_qty,
-#                     'date_planned': date.today(),
-#                     'size': line.store_request_size,
-#                     'brand_id': line.brand_id.id,
-#                     'certificate_required': line.certificate_required,
-#                     'price': line.product_id.standard_price,
-#                 }))
+    def action_assign_owner(self):
+        pass
 
-#         res = {
-#             'type': 'ir.actions.act_window',
-#             'name': ('ATP FORM'),
-#             'res_model': 'atp.form',
-#             'view_type': 'form',
-#             'view_mode': 'form',
-#             'view_id': view_id,
-#             'target': 'current',
-#             'context': {'default_project_des': self.project_description, 'default_stock_source': self.name, 'default_expected_date': self.scheduled_date, 'default_atp_form_line_ids': order_lines}
-#         }
-#         return res
+    def create_atp_order(self):
+        """
+        Method to open create atp form
+        """
+        view_ref = self.env['ir.model.data'].get_object_reference(
+            'topline', 'topline_atp_form_view')
+        view_id = view_ref[1] if view_ref else False
+
+        for subscription in self:
+            order_lines = []
+            for line in subscription.move_ids_without_package:
+                order_lines.append((0, 0, {
+                    'name': line.product_id.name,
+                    'product_id': line.product_id.id,
+                    'qty': line.product_uom_qty,
+                    'date_planned': date.today(),
+                    'size': line.store_request_size,
+                    'brand_id': line.brand_id.id,
+                    'certificate_required': line.certificate_required,
+                    'price': line.product_id.standard_price,
+                }))
+
+        res = {
+            'type': 'ir.actions.act_window',
+            'name': ('ATP FORM'),
+            'res_model': 'atp.form',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': view_id,
+            'target': 'current',
+            'context': {'default_project_des': self.project_description, 'default_stock_source': self.name, 'default_expected_date': self.scheduled_date, 'default_atp_form_line_ids': order_lines}
+        }
+        return res
+
 
 class StockRejectionReason(models.Model):
     _name = "stock.rejection.reason"
@@ -271,4 +264,3 @@ class StockPickingRejection(models.TransientModel):
             self.env.context.get('active_ids'))
         leads.write({'rejection_reason': self.rejection_reason_id.id})
         return leads.button_reject()
-    
